9cd4d77c66f32bb0423774c86564f14dd02f5c8c
Trevor Cickovski <movingpictures83@gmail.com>
2024-01-03 15:13:24 -0500

    Changed from 32-bit to 5-bit

--- Sixteen.v
+++ Sixteen.v
@@ -15,43 +15,16 @@
 
 // PROGRAM		"Quartus Prime"
 // VERSION		"Version 20.1.1 Build 720 11/11/2020 SJ Lite Edition"
-// CREATED		"Wed Jan 03 14:30:22 2024"
+// CREATED		"Wed Jan 03 15:12:08 2024"
 
 module Sixteen(
 	Y
 );
 
 
-output wire	[31:0] Y;
+output wire	[4:0] Y;
 
-wire	[31:0] Y_ALTERA_SYNTHESIZED;
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+wire	[4:0] Y_ALTERA_SYNTHESIZED;
 
 
 
@@ -65,33 +38,6 @@ assign	Y = Y_ALTERA_SYNTHESIZED;
 assign	Y_ALTERA_SYNTHESIZED[0] = 0;
 assign	Y_ALTERA_SYNTHESIZED[1] = 0;
 assign	Y_ALTERA_SYNTHESIZED[3] = 0;
-assign	Y_ALTERA_SYNTHESIZED[5] = 0;
-assign	Y_ALTERA_SYNTHESIZED[6] = 0;
-assign	Y_ALTERA_SYNTHESIZED[7] = 0;
-assign	Y_ALTERA_SYNTHESIZED[8] = 0;
-assign	Y_ALTERA_SYNTHESIZED[9] = 0;
-assign	Y_ALTERA_SYNTHESIZED[10] = 0;
-assign	Y_ALTERA_SYNTHESIZED[11] = 0;
-assign	Y_ALTERA_SYNTHESIZED[12] = 0;
-assign	Y_ALTERA_SYNTHESIZED[13] = 0;
-assign	Y_ALTERA_SYNTHESIZED[14] = 0;
-assign	Y_ALTERA_SYNTHESIZED[15] = 0;
-assign	Y_ALTERA_SYNTHESIZED[16] = 0;
-assign	Y_ALTERA_SYNTHESIZED[17] = 0;
-assign	Y_ALTERA_SYNTHESIZED[18] = 0;
-assign	Y_ALTERA_SYNTHESIZED[19] = 0;
-assign	Y_ALTERA_SYNTHESIZED[20] = 0;
-assign	Y_ALTERA_SYNTHESIZED[21] = 0;
-assign	Y_ALTERA_SYNTHESIZED[22] = 0;
-assign	Y_ALTERA_SYNTHESIZED[23] = 0;
-assign	Y_ALTERA_SYNTHESIZED[24] = 0;
-assign	Y_ALTERA_SYNTHESIZED[25] = 0;
-assign	Y_ALTERA_SYNTHESIZED[26] = 0;
-assign	Y_ALTERA_SYNTHESIZED[27] = 0;
-assign	Y_ALTERA_SYNTHESIZED[28] = 0;
-assign	Y_ALTERA_SYNTHESIZED[29] = 0;
-assign	Y_ALTERA_SYNTHESIZED[30] = 0;
-assign	Y_ALTERA_SYNTHESIZED[31] = 0;
 assign	Y_ALTERA_SYNTHESIZED[4] = 1;
 assign	Y_ALTERA_SYNTHESIZED[2] = 0;
 
